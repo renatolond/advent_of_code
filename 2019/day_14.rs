4a1a53d6a9ef5498b1ed47518ad4c7a640566423
Renato "Lond" Cerqueira <renato@lond.com.br>
2019-12-22 16:53:35 +0100

    2019: Day 14 p1

--- 2019/day_14.rs
+++ 2019/day_14.rs
@@ -1,14 +1,19 @@
 use std::process;
 use std::io;
 use std::io::prelude::*;
-use std::convert::TryInto;
-use std::collections::HashSet;
+use std::fmt;
 
-#[derive(Debug)]
 struct Material {
     name: String,
     quantity: i32
 }
+
+impl fmt::Debug for Material {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}*{}", self.quantity, self.name)
+    }
+}
+
 
 impl PartialEq for Material {
     fn eq(&self, other: &Self) -> bool {
@@ -26,17 +31,72 @@ static mut ORE_EQUATIONS : Vec<usize> = Vec::new();
 
 static mut BASE_MATERIALS : Vec<String> = Vec::new();
 
-static mut equation_queue : Vec<usize> = Vec::new();
+unsafe fn print_equation(equation_idx : usize) {
+    let equation = &EQUATIONS[equation_idx];
+    for element in equation.left_side.iter() {
+        print!("{:?} ", element);
+    }
+    print!("= ");
+    for element in equation.right_side.iter() {
+        print!("{:?} ", element);
+    }
+    println!("");
+}
+
+unsafe fn eliminate_extras(equation_idx : usize) {
+    let main_equation = &mut EQUATIONS[equation_idx];
+    for left_element in main_equation.left_side.iter_mut() {
+        for right_element in main_equation.right_side.iter_mut() {
+            if right_element.name != left_element.name {
+                continue;
+            }
+            if right_element.quantity > left_element.quantity {
+                right_element.quantity -= left_element.quantity;
+                left_element.quantity = 0;
+            } else {
+                left_element.quantity -= right_element.quantity;
+                right_element.quantity = 0;
+            }
+        }
+    }
+    {
+        let mut left_element_idx = 0;
+        loop {
+            if main_equation.left_side[left_element_idx].quantity == 0 {
+                main_equation.left_side.remove(left_element_idx);
+            } else {
+                left_element_idx += 1;
+            }
+            if left_element_idx >= main_equation.left_side.len() {
+                break
+            }
+        }
+    }
+    {
+        let mut right_element_idx = 0;
+        loop {
+            if main_equation.right_side[right_element_idx].quantity == 0 {
+                main_equation.right_side.remove(right_element_idx);
+            } else {
+                right_element_idx += 1;
+            }
+            if right_element_idx >= main_equation.right_side.len() {
+                break
+            }
+        }
+    }
+}
 
 unsafe fn reduce_equation(main_equation_idx : usize, ignore_ores : bool) {
     // Step 1: Convert all non-ORE elements into ORE elements
     let mut element_idx = 0;
     loop {
-        println!("Looking at #{} : {:?}", element_idx, EQUATIONS[main_equation_idx].left_side[element_idx]);
         if element_idx >= EQUATIONS[main_equation_idx].left_side.len() {
             break;
         }
 
+        print_equation(main_equation_idx);
+        println!("Looking at #{} : {:?}", element_idx, EQUATIONS[main_equation_idx].left_side[element_idx]);
         if EQUATIONS[main_equation_idx].left_side[element_idx].name == "ORE" {
             println!("panic?");
             process::exit(2);
@@ -79,20 +139,16 @@ unsafe fn reduce_equation(main_equation_idx : usize, ignore_ores : bool) {
 
         {
             let equation_idx = next_idx.unwrap();
-            let mut material : Material;
-            let mut main_equation = &mut EQUATIONS[main_equation_idx];
+            let material : Material;
+            let main_equation = &mut EQUATIONS[main_equation_idx];
             println!("#{}",element_idx);
             material = main_equation.left_side.remove(element_idx);
             let multiplier;
-            if material.quantity % EQUATIONS[equation_idx].right_side[0].quantity != 0 {
-                println!("Equation {} needs to be reduced!", equation_idx);
-                equation_queue.push(equation_idx);
-                continue;
-            } else {
-                multiplier = material.quantity / EQUATIONS[equation_idx].right_side[0].quantity;
-            }
-            println!("Chose {} * {}", material.name, multiplier);
-            assert_eq!(material.quantity % EQUATIONS[equation_idx].right_side[0].quantity, 0);
+            multiplier = (material.quantity as f32 / EQUATIONS[equation_idx].right_side[0].quantity as f32).ceil() as i32;
+            let left_over = (EQUATIONS[equation_idx].right_side[0].quantity * multiplier) % material.quantity;
+            let left_quantity = (EQUATIONS[equation_idx].right_side[0].quantity * multiplier) - left_over;
+            let right_quantity = left_over;
+            println!("Chose {} * {} ( {} = {} ) - ( {} / {} )", material.name, multiplier, left_quantity, right_quantity, material.quantity, EQUATIONS[equation_idx].right_side[0].quantity);
             for element in EQUATIONS[equation_idx].left_side.iter() {
                 if main_equation.left_side.contains(&element) {
                     for el in main_equation.left_side.iter_mut() {
@@ -105,17 +161,26 @@ unsafe fn reduce_equation(main_equation_idx : usize, ignore_ores : bool) {
                     main_equation.left_side.push( Material { name: element.name.clone(), quantity: element.quantity * multiplier } );
                 }
             }
-
+            if left_over > 0 {
+                if main_equation.right_side.contains(&material) {
+                    for el in main_equation.right_side.iter_mut() {
+                        if el.name == material.name {
+                            el.quantity += left_over;
+                            break;
+                        }
+                    }
+                } else {
+                    main_equation.right_side.push( Material { name: material.name.clone(), quantity: left_over } );
+                }
+            }
         }
-        for element in EQUATIONS[main_equation_idx].left_side.iter() {
-            println!("{:?}", element);
-        }
-        println!("Ore equations: {}", ORE_EQUATIONS.len());
+        eliminate_extras(main_equation_idx);
     }
 }
 
 unsafe fn real_main() {
-    let mut fuel_equation_idx = 9999999;
+    let fuel_equation_idx = 0;
+    EQUATIONS.push(Equation { left_side: Vec::new(), right_side: Vec::new() });
     for line in io::stdin().lock().lines() {
         let line_str: String = line.unwrap();
         let parts : Vec<&str> = line_str.split(" => ").collect();
@@ -133,26 +198,48 @@ unsafe fn real_main() {
             left.push(Material { name: name, quantity: qty });
         }
         let mut right : Vec<Material> = Vec::new();
-        let mut is_fuel_equation = false;
         for material in parts[1].split(", ") {
             let mat_parts : Vec<&str> = material.split_whitespace().collect();
             assert_eq!(mat_parts.len(), 2);
             let qty : i32 = mat_parts[0].parse::<i32>().unwrap();
             let name  = mat_parts[1].to_string();
-            if name == "FUEL" {
-                is_fuel_equation = true;
-            }
             right.push(Material { name: name, quantity: qty });
         }
         if is_ore_equation {
             ORE_EQUATIONS.push(EQUATIONS.len());
+        } else {
+            let main_equation = &mut EQUATIONS[fuel_equation_idx];
+            for element in left.iter() {
+                if main_equation.left_side.contains(&element) {
+                    for el in main_equation.left_side.iter_mut() {
+                        if el.name == element.name {
+                            el.quantity += element.quantity;
+                            break;
+                        }
+                    }
+                } else {
+                    main_equation.left_side.push( Material { name: element.name.clone(), quantity: element.quantity} );
+                }
+            }
+            for element in right.iter() {
+                if main_equation.right_side.contains(&element) {
+                    for el in main_equation.right_side.iter_mut() {
+                        if el.name == element.name {
+                            el.quantity += element.quantity;
+                            break;
+                        }
+                    }
+                } else {
+                    main_equation.right_side.push( Material { name: element.name.clone(), quantity: element.quantity} );
+                }
+            }
+            print_equation(fuel_equation_idx);
         }
-        if is_fuel_equation {
-            fuel_equation_idx = EQUATIONS.len();
-        }
-
         EQUATIONS.push(Equation { left_side: left, right_side: right });
     }
+
+    eliminate_extras(fuel_equation_idx);
+    print_equation(fuel_equation_idx);
 
     for equation_idx in ORE_EQUATIONS.iter() {
         assert_eq!(EQUATIONS[*equation_idx].right_side.len(), 1);
@@ -160,20 +247,12 @@ unsafe fn real_main() {
     }
 
     reduce_equation(fuel_equation_idx, true);
-    while equation_queue.len() > 0 {
-        let equation_idx = equation_queue.remove(0);
-        println!("Reducing equation {}", equation_idx);
-        reduce_equation(equation_idx, false);
-    }
+    print_equation(fuel_equation_idx);
 
     println!("Fuel equation seems reduced. Let's move forward");
 
     for ore_equation_idx in ORE_EQUATIONS.iter() {
-        let ore_equation = &EQUATIONS[*ore_equation_idx];
-        for element in ore_equation.left_side.iter() {
-            print!("{:?} ", element);
-        }
-        println!("= {:?}", ore_equation.right_side[0]);
+        print_equation(*ore_equation_idx);
     }
 
 
